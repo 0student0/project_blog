ab2248b6d01a81e046b4f12b74d9c9d215f6c579
Sergey Kolesnik <orion220782@gmail.com>
2021-02-01 23:40:30 +0300

    home page created

--- Config/settings.py
+++ Config/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'blog'
+    'blog',
 ]
 
 MIDDLEWARE = [
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'Config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 
+from blog import views
+
 urlpatterns = [
-    path('', )
+    path('', views.HomePageView.as_view(), name='home_page'),
 ]
